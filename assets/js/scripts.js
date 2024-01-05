7eefbaf38004cac5d0cfc77387fee7ac8c92d87a
Sadbin Shakil <sadbin7979@gmail.com>
2024-01-05 17:49:03 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,36 @@
 $(document).ready(function(){
 	// Mobile Menu
 	$('.menu_icon').click(function(){
-		$('.menu').slideToggle();
+		$('.menu').slideToggle(); 
 
-		return false
+		return false;
+	});
+
+	$('.mobile_bars').click(function(){
+		$('.menu_bar_overlay').fadeIn();
+		$('.header_mobile_menu').fadeIn();
+
+		return false;
+	});
+
+	$('.menu_bar_overlay').click(function(){
+		$('.menu_bar_overlay').fadeOut();
+		$('.header_mobile_menu').fadeOut(); 
+	});
+
+	$('.filter_modal').click(function(){
+		$('.search_filter_modal').show();
+		$('.modal_overlay').show(); 
+	});
+
+	$('.modal_close').click(function(){
+		$('.search_filter_modal').hide();
+		$('.modal_overlay').hide(); 
+	});
+
+	$('.modal_overlay').click(function(){
+		$('.search_filter_modal').hide();
+		$('.modal_overlay').hide(); 
 	});
 
 	$(document).ready(function(){
@@ -34,6 +61,27 @@ $(document).ready(function(){
 		  }
 	});
 
+	$('.modal_carousel_main').owlCarousel({
+		items: 5,
+		loop:true,
+		margin:14,
+		dots: false,
+		nav:true,
+		autoplay:false,
+		navText: ["","<img src='assets/images/search/arrow.png'>"] ,
+		responsive:{
+			0:{
+			  items:3
+			},
+			450:{
+				items: 3,
+			},
+			700:{
+				items: 5,
+			}
+		  }
+	});
+
 	 
 	$('.ideal_carousel').owlCarousel({
 		items:1,
@@ -41,7 +89,15 @@ $(document).ready(function(){
 		nav:false,
 		dots:true,
 		margin:20,
-		autoplay:true, 
+		// autoplay:true, 
+		responsive:{
+			0:{
+				dots:false,
+			  },
+			  767:{
+				dots:true,
+			  }
+			}
 	});
 	$('.service_mobile_carousel').owlCarousel({
 		items:1.5,
